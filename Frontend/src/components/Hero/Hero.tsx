0fedec27147aefea0af94e0bafb84ca1c57a7852
ahsanali17 <ahsantime1@gmail.com>
2022-12-18 18:24:40 -0500

    Hero section has begun

---
+++ Frontend/src/components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {HeroSection} from './styles';
+
+function Hero() {
+  return (
+    <>
+     <HeroSection backgroundColor="#3498db" fontColor="#ffffff">
+      <h1>Welcome to my website!</h1>
+    </HeroSection>
+    </>
+  )
+}
+
+export default Hero;
