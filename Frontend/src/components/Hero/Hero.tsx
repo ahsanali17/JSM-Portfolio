56ac38e5f2c9ff25269ab00f43264b1e795d9f7a
ahsanali17 <ahsantime1@gmail.com>
2022-12-21 04:35:57 -0500

    cleaned up hero component and its styles' file

--- Frontend/src/components/Hero/Hero.tsx
+++ Frontend/src/components/Hero/Hero.tsx
@@ -1,38 +1,26 @@
-import React, { useRef, useEffect } from 'react';
-import Image from "next/image";
+import React from 'react';
 
 import {TypedReactAnimation} from '..';
-import { HeroPicture, HeroPictureBlock, HeroPictureContainer, HeroSection, HeroSectionImage, HeroText, HeroText2, HeroTextPartition } from './styles';
 import { HeroProfilePic } from '../../assets';
+import { HeroPicture, HeroPictureBlock, HeroPictureContainer, HeroSection, HeroSectionImage, HeroText, HeroTextPartition } from './styles';
 
 function Hero() {
-  const stringArray = [
-    'Some <i>strings</i> are slanted',
-    'Some <strong>strings</strong> are bold',
-    'HTML characters &times; &copy;'
-  ]
   
   const nameString = [
-    'Hello world, my name is Ahsan Ali Syed'
+    "Hello world, my name is Ahsan Ali Syed<br/>I'm a Software Developer from Montreal, Canada.<br/>I build ReactJS full-stack applications with modern technologies.<br/>On my free time I write smart contracts.",
   ]
-  
-  const imageUrl = 'Frontend/src/assets/ahsan-ai-2.png';
   
   return (
     <HeroSection backgroundColor="#3498db" fontColor="#ffffff">
       <HeroTextPartition>
         <HeroText>
-          <TypedReactAnimation stringArray={nameString} typingSpeed={60} backingSpeed={20} animationLoop={false} />
+          <TypedReactAnimation stringArray={nameString} typingSpeed={40} backingSpeed={20} animationLoop={false} />
         </HeroText>
-        <HeroText2>
-          I'm a Software Developer from Montreal, Canada. <br/>I build ReactJS full-stack applications with modern technologies. <br/> On my free time I write smart contracts.   
-        </HeroText2>
       </HeroTextPartition>
       <HeroPicture>
         <HeroPictureBlock>
           <HeroPictureContainer>
-           
-            <HeroSectionImage src={HeroProfilePic} alt='my hero sections profile picture' height={400} width={500}/>
+            <HeroSectionImage src={HeroProfilePic} alt='My hero sections profile picture' height={400} width={500}/>
           </HeroPictureContainer>
         </HeroPictureBlock>
       </HeroPicture>
