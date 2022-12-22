d480f57107c5138b64efef57d245ec51a64babf8
ahsanali17 <ahsantime1@gmail.com>
2022-12-22 02:03:03 -0500

    began working on about section

---
+++ Frontend/src/components/About/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {AboutSection} from './styles'
+
+function About() {
+  return (
+    <AboutSection backgroundColor='#151616' fontColor='black'>
+     <h1>yo</h1>
+    </AboutSection>
+  )
+}
+
+export default About;
