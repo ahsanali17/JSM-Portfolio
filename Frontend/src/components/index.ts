0361a296d16b61859fcf4f85de48a99ec6a20254
ahsanali17 <ahsantime1@gmail.com>
2022-12-22 23:41:30 -0500

    more styling changes and some file changes as well

--- Frontend/src/components/index.ts
+++ Frontend/src/components/index.ts
@@ -3,7 +3,6 @@ export { default as Layout} from './Layout/Layout';
 export { default as LeftSidebar} from './LeftSidebar/LeftSidebar';
 export { default as RightSidebar} from './RightSidebar/RightSidebar';
 export { default as Navbar } from './Navbar/Navbar';
-export { default as NavbarData } from './Navbar/NavbarData'; 
 export { default as NavbarLogo } from './Navbar/Logo/Logo';
 export { default as NavbarMenuToggle } from './Navbar/NavbarMenuToggle/NavbarMenuToggle';
 export { default as Footer } from './Footer/Footer';
