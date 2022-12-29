afd8cafcc52353d7293e8038f2703125cfcb7c1a
ahsanali17 <ahsantime1@gmail.com>
2022-12-29 00:23:07 -0500

    almost done the about section

--- Frontend/src/components/About/About.tsx
+++ Frontend/src/components/About/About.tsx
@@ -1,25 +1,56 @@
 import React from 'react'
+import {SiJavascript, SiSolidity, SiReact, SiNextdotjs, SiVite, SiRubyonrails,SiTypescript, SiRedux, SiMysql, SiGraphql, SiMongodb, SiJest, FaHardHat } from 'react-icons';
 
-import {AboutMeText, AboutSection, AboutSectionPicture, TechnologyStack} from './styles'
+
+import {AboutSection, AboutMeTextAndPicture, AboutSectionPictureWrapper, AboutSectionPicture, AboutMeTextWrapper, AboutMeText, TechnologyStack, TechnologyStackIconContainer} from './styles'
 import { HeroProfilePic } from '../../assets';
 
 function About() {
+  const techStackData = [
+    {icon: SiJavascript, iconName: 'Javascript'},
+    {icon: SiSolidity, iconName: 'Solidity'},
+    {icon: SiReact, iconName: 'React'},
+    {icon: SiNextdotjs, iconName: 'Next.js'},
+    {icon: SiVite, iconName: 'Vite'},
+    {icon: SiRubyonrails, iconName: 'Ruby on Rails'},
+    {icon: SiTypescript, iconName: 'Typescript'},
+    {icon: SiRedux, iconName: 'Redux'},
+    {icon: SiMysql, iconName: 'MySql'},
+    {icon: SiGraphql, iconName: 'GraphQl'},
+    {icon: SiMongodb, iconName: 'MongoDB'},
+    {icon: SiJest, iconName: 'Jest'},
+    {icon: FaHardHat, iconName: 'Hardhat'},
+  ] 
+  
+  
+  
   return (
-    <AboutSection backgroundColor='black' fontColor='white'>
+    <AboutSection className='#About' backgroundColor='black' fontColor='white'>
      
-     <div>
-      <AboutSectionPicture src={HeroProfilePic} alt='My About Section picture' height={250} width={300} />
-      
-     </div>
-     
-     <AboutMeText>
-      I started my journey in software engineering back in 2019 doing tutorials on Udemy.<br/>After getting more serious about it I decided to commit fully and jump into a bootcamp to begin learning full stack development.<br/> I got hired and began working as a trainee Full stack developer(React, Ruby on Rails, GraphQl).<br/>Since then I have continued my journey, currently I am exploring the world of Typescript.
-      </AboutMeText>
+     <AboutMeTextAndPicture>
+        <AboutSectionPictureWrapper>
+          <AboutSectionPicture src={HeroProfilePic} alt='My About Section picture' height={250} width={300} />
+        </AboutSectionPictureWrapper>
+        
+        <AboutMeTextWrapper>
+          <AboutMeText>
+            I started my journey in software engineering back in 2019 doing tutorials on Udemy.<br/><br/>After getting more serious about it I decided to commit fully and jump into a bootcamp to begin learning full stack development. <br/><br/>I got hired and began working as a trainee Full stack developer (React, Ruby on Rails, GraphQl). Since then I have continued my journey, currently I am exploring the world of Typescript and aiming to become a smart contract developer.
+          </AboutMeText>
+        </AboutMeTextWrapper>
+      </AboutMeTextAndPicture>
       
       
       <h2>Technologies & Tools I work with</h2>
       <TechnologyStack>
-        
+          {techStackData.map((item, index) => (
+            <TechnologyStackIconContainer key={index}>
+              <span>
+                {item.iconName}  
+              </span>
+            </TechnologyStackIconContainer>
+            
+            )
+          )}
         
       </TechnologyStack>
     </AboutSection>
