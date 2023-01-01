c39fb835af8c3fe94605d9eef950f8913a182e66
ahsanali17 <ahsantime1@gmail.com>
2023-01-01 17:09:35 -0500

    made some more styling changes, I'm happy with the hero section being mobile responsive and color scheme is coming in

--- Frontend/src/components/Hero/styles.ts
+++ Frontend/src/components/Hero/styles.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Image from "next/image";
 
 interface HeroProps {
  backgroundColor: string;
@@ -12,68 +11,65 @@ export const HeroSection = styled.section<HeroProps>`
   align-content: center;
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  grid-gap: 50px;
-  gap: 50px;
+  gap: 10px;
   justify-content: center;
-  margin: 40px;
-  min-height: 70vh;
+  padding: 20px 100px;
+  
+  min-height: 94vh;
+  
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const HeroTextPartition = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   justify-content: center;
-  max-width: 711px;
-  width: 45vw;
-  
 `;
 
 export const HeroText = styled.h1`
-  margin: 15px 0;
+  font-size: 20px;
+  width: 500px;
+  
+  @media screen and (max-width: 700px) {
+    width: 100%;
+  }
+  
+  @media screen and (max-width: 600px) {
+    width: 100%;
+  } 
+  
+  @media screen and (max-width: 500px) {
+    width: 90%;
+  } 
 `;
 
 export const HeroPicture = styled.div`
-  -webkit-animation: home_slideIn__3MLMw 7s;
-  animation: {
-    animation-duration: 7s;
-    animation-timing-function: ease;
-    animation-delay: 0s;
-    animation-iteration-count: 1;
-    animation-direction: normal;
-    animation-fill-mode: none;
-    animation-play-state: running;
-    animation-name: home_slideIn__3MLMw;
-  };
-  border-radius: 10%;
-  overflow: hidden;
-  width: 400px;
-`;
-
-export const HeroPictureBlock = styled.div`
-  display: inline-block;
-  max-width: 100%;
-  overflow: hidden;
-  position: relative;
-  box-sizing: border-box;
-  margin: 0;
+  display: inherit;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  
+  @media screen and (min-width: 320px) {
+    width: 300px;
+    
+  }
 `;
 
 export const HeroPictureContainer = styled.div`
-  box-sizing: border-box;
-  display: block;
-  max-width: 100%;
-`;
-
-export const HeroSectionImage = styled(Image)`
-  max-width: 100%;
-  display: block;
-  margin: 0;
-  border: none;
-  padding: 0;
+  position: relative;
+  width: 500px;
+  height: 400px;
   
-  min-width: 100%;
-  max-width: 100%;
-  min-height: 100%;
-  max-height: 100%;
-`;
+  @media screen and (max-width: 600px) {
+    width: 100%;
+  }
+  
+  @media screen and (max-width: 320px) {
+    width: 100%;
+  }
+`; 
