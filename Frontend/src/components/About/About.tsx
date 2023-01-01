60b93eb1f5887db05f3701b647619f9e3ddcdc7b
ahsanali17 <ahsantime1@gmail.com>
2023-01-01 11:28:19 -0500

    added in last component of about section

--- Frontend/src/components/About/About.tsx
+++ Frontend/src/components/About/About.tsx
@@ -1,28 +1,30 @@
 import React from 'react'
-import {SiJavascript, SiSolidity, SiReact, SiNextdotjs, SiVite, SiRubyonrails,SiTypescript, SiRedux, SiMysql, SiGraphql, SiMongodb, SiJest, FaHardHat } from 'react-icons';
+import {SiJavascript, SiSolidity, SiReact, SiNextdotjs, SiVite, SiRubyonrails,SiTypescript, SiRedux, SiMysql, SiGraphql, SiMongodb, SiJest } from 'react-icons/si';
+import {FaHardHat} from 'react-icons/fa';
+import {Tooltip} from 'react-tooltip';
 
-
-import {AboutSection, AboutMeTextAndPicture, AboutSectionPictureWrapper, AboutSectionPicture, AboutMeTextWrapper, AboutMeText, TechnologyStack, TechnologyStackIconContainer} from './styles'
+import {AboutSection, AboutMeTextAndPicture, AboutSectionPictureWrapper, AboutSectionPicture, AboutMeTextWrapper, AboutMeText, TechnologyStack, TechnologyStackHeader, TechnologyStackIconContainer, TechnologyStackIcons} from './styles'
 import { HeroProfilePic } from '../../assets';
+import { TooltipTarget } from './Tooltip/styles';
 
 function About() {
   const techStackData = [
-    {icon: SiJavascript, iconName: 'Javascript'},
-    {icon: SiSolidity, iconName: 'Solidity'},
-    {icon: SiReact, iconName: 'React'},
-    {icon: SiNextdotjs, iconName: 'Next.js'},
-    {icon: SiVite, iconName: 'Vite'},
-    {icon: SiRubyonrails, iconName: 'Ruby on Rails'},
-    {icon: SiTypescript, iconName: 'Typescript'},
-    {icon: SiRedux, iconName: 'Redux'},
-    {icon: SiMysql, iconName: 'MySql'},
-    {icon: SiGraphql, iconName: 'GraphQl'},
-    {icon: SiMongodb, iconName: 'MongoDB'},
-    {icon: SiJest, iconName: 'Jest'},
-    {icon: FaHardHat, iconName: 'Hardhat'},
+    {icon: <SiJavascript />, iconName: 'Javascript'},
+    {icon: <SiSolidity />, iconName: 'Solidity'},
+    {icon: <SiReact />, iconName: 'React'},
+    {icon: <SiNextdotjs />, iconName: 'Next.js'},
+    {icon: <SiVite />, iconName: 'Vite'},
+    {icon: <SiRubyonrails />, iconName: 'Ruby on Rails'},
+    {icon: <SiTypescript />, iconName: 'Typescript'},
+    {icon: <SiRedux />, iconName: 'Redux'},
+    {icon: <SiMysql />, iconName: 'MySql'},
+    {icon: <SiGraphql />, iconName: 'GraphQl'},
+    {icon: <SiMongodb />, iconName: 'MongoDB'},
+    {icon: <SiJest />, iconName: 'Jest'},
+    {icon: <FaHardHat />, iconName: 'Hardhat'},
   ] 
   
-  
+  // const firstFive = techStackData.slice(0,6);
   
   return (
     <AboutSection className='#About' backgroundColor='black' fontColor='white'>
@@ -40,21 +42,29 @@ function About() {
       </AboutMeTextAndPicture>
       
       
-      <h2>Technologies & Tools I work with</h2>
       <TechnologyStack>
+        <TechnologyStackHeader>
+          Technologies & Tools I work with
+        </TechnologyStackHeader>
+        <TechnologyStackIconContainer>
           {techStackData.map((item, index) => (
-            <TechnologyStackIconContainer key={index}>
-              <span>
-                {item.iconName}  
-              </span>
-            </TechnologyStackIconContainer>
-            
-            )
-          )}
-        
+            <div key={index}>
+              <TechnologyStackIcons>
+                {item.icon}
+              </TechnologyStackIcons>
+            </div>
+          ))}
+            <Tooltip text="hey" position="top">
+              <TooltipTarget>I am target</TooltipTarget>
+            </Tooltip>
+          </TechnologyStackIconContainer>
       </TechnologyStack>
     </AboutSection>
   )
 }
 
 export default About;
+
+function useKey() {
+  throw new Error('Function not implemented.');
+}
