ab3700f4db88821da3774b2e743c958402390d89
ahsanali17 <ahsantime1@gmail.com>
2023-01-10 15:37:52 -0500

    major styling issues with mobile responsiveness, finally fixed

--- Frontend/src/components/Hero/Hero.tsx
+++ Frontend/src/components/Hero/Hero.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 
 import {TypedReactAnimation} from '..';
 import { HeroProfilePic } from '../../assets';
-import { HeroPicture, HeroPictureBlock, HeroPictureContainer, HeroSection, HeroSectionImage, HeroText, HeroTextPartition } from './styles';
+import { HeroMainContainer, HeroPicture, HeroPictureContainer, HeroSection, HeroText, HeroTextPartition } from './styles';
 
 function Hero() {
-  
+
   const nameString = [
     "Hello world, my name is Ahsan Ali Syed<br/>I'm a Software Developer from Montreal, Canada.<br/>I build ReactJS full-stack applications with modern technologies.<br/>On my free time I write smart contracts.",
   ]
-  
+
   return (
-    <HeroSection backgroundColor="#3498db" fontColor="#ffffff">
-      <HeroTextPartition>
-        <HeroText>
-          <TypedReactAnimation stringArray={nameString} typingSpeed={40} backingSpeed={20} animationLoop={false} />
-        </HeroText>
-      </HeroTextPartition>
-      <HeroPicture>
-        <HeroPictureBlock>
-          <HeroPictureContainer>
-            <HeroSectionImage src={HeroProfilePic} alt='My hero sections profile picture' height={400} width={500}/>
-          </HeroPictureContainer>
-        </HeroPictureBlock>
-      </HeroPicture>
+    <HeroSection backgroundColor="#000ff906" fontColor="#ffffff">
+      <HeroMainContainer>
+
+        <HeroTextPartition>
+          <HeroText>
+            <TypedReactAnimation stringArray={nameString} typingSpeed={40} backingSpeed={20} animationLoop={false}
+            style={{width:"100px", color: "#ffcc", fontSize:"1.8rem"}}
+            />
+          </HeroText>
+        </HeroTextPartition>
+        <HeroPicture>
+            <HeroPictureContainer>
+              <Image src={HeroProfilePic} alt='My hero sections profile picture' fill style={{borderRadius: '20px', boxShadow: "5px 5px 50px #7d7979"}} />
+            </HeroPictureContainer>
+        </HeroPicture>
+
+      </HeroMainContainer>
     </HeroSection>
   );
 }
