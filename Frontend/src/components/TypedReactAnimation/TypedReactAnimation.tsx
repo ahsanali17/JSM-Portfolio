26e54379fecd33e183a6ed3366cc293ae45c4c17
Ahsan Ali Syed <ahsan.syed@codeboxx.biz>
2023-04-20 12:32:46 -0400

    Update TypedReactAnimation.tsx

--- Frontend/src/components/TypedReactAnimation/TypedReactAnimation.tsx
+++ Frontend/src/components/TypedReactAnimation/TypedReactAnimation.tsx
@@ -8,42 +8,46 @@ interface typeAnimationProps {
  animationLoop: boolean
 }
 
-const TypedReactAnimation: FC<typeAnimationProps> = ({stringArray, typingSpeed, backingSpeed, animationLoop}) => {
- // Create reference to store the DOM element containing the animation
- const el = useRef(null);
- // Create reference to store the Typed instance itself
- const typed = useRef(null);
+const TypedReactAnimation: FC<typeAnimationProps> = ({
+  stringArray,
+  typingSpeed,
+  backingSpeed,
+  animationLoop,
+}) => {
+  // Create reference to store the DOM element containing the animation
+  const el = useRef<HTMLSpanElement | null>(null)
+  // Create reference to store the Typed instance itself
+  const typed = useRef<Typed | null>(null)
 
- useEffect(() => {
-   const options = {
-     strings: stringArray,
-     typeSpeed: typingSpeed,
-     backSpeed: backingSpeed,
-     loop: animationLoop
-   };
+  useEffect(() => {
+    const options = {
+      strings: stringArray,
+      typeSpeed: typingSpeed,
+      backSpeed: backingSpeed,
+      loop: animationLoop,
+    }
 
-   // Assign the DOM element to the current property of the el ref object
-   el.current = document.querySelector('.type-wrap > span');
+    // Assign the DOM element to the current property of the el ref object
+    if (el.current) {
+      typed.current = new Typed(el.current, options)
+    }
 
-   // Assign the new Typed instance to the current property of the typed ref object
-   typed.current = new Typed(el.current, options);
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      if (typed.current) {
+        typed.current.destroy()
+      }
+    }
+  }, [])
 
-   return () => {
-     // Make sure to destroy Typed instance during cleanup
-     // to prevent memory leaks
-     typed.current.destroy();
-   };
- }, []);
-
- return (
-   <div className="wrap">
-     {/* <h1>Typed.js</h1> */}
-
-     <div className="type-wrap">
-       <span style={{ whiteSpace: 'pre' }} ref={el} />
-     </div>
-   </div>
- );
-};
+  return (
+    <div className="wrap">
+      <div className="type-wrap">
+        <span style={{ whiteSpace: 'pre' }} ref={el} />
+      </div>
+    </div>
+  )
+}
 
 export default TypedReactAnimation
