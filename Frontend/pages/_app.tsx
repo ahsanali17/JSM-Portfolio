b5b996edf26636b42cac8e63ff71e7a69e98fef0
ahsanali17 <ahsantime1@gmail.com>
2023-10-13 17:25:53 -0400

    fixes

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -4,9 +4,11 @@ import { Layout } from '../src/components/index';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>{
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  }</>
+  return (
+    <>
+      <Layout>
+        {...pageProps}
+      </Layout>
+    </>
+  )
 }
