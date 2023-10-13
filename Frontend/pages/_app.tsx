5adf5225fef7a47d8e49b8c36fd7f34a7676a0d9
ahsanali17 <ahsantime1@gmail.com>
2023-10-13 17:42:18 -0400

    fixing display issue

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -1,14 +1,15 @@
+//@ts-nocheck
 import type { AppProps } from 'next/app';
 
 import { Layout } from '../src/components/index';
 import '../styles/globals.css'
-import { Fragment } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layout>
-        <Fragment {...pageProps} />
+        {/* //@ts-ignore */}
+        <Component {...pageProps} />
       </Layout>
     </>
   )
